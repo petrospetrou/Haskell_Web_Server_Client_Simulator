f13a4522547374feb5078fbc12c0c55595374dd1
Petros Petrou <p.petrou@se24.qmul.ac.uk>
2025-01-09 20:55:50 +0000

    Created the Data Types

--- src/Types.hs
+++ src/Types.hs
@@ -1,1 +1,18 @@
-module Types () where
+module Types where
+
+import Control.Concurrent (MVar)
+import Data.Time (UTCTime)
+
+-- | Data type for an HTTP-like request
+data Request = Request 
+  { reqTimestamp :: UTCTime
+  , reqContent   :: String
+  }
+  deriving (Show)
+
+-- | Data type for an HTTP-like response
+data Response = Response
+  { resTimestamp :: UTCTime
+  , resContent   :: String
+  }
+  deriving (Show)
