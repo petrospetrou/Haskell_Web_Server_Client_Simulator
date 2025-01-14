f7b2fecad50e36693f3fd0f093f457afab741151
Petros Petrou <p.petrou@se24.qmul.ac.uk>
2025-01-14 14:44:55 +0000

    Adding the correct comments to the Types.hs

--- src/Types.hs
+++ src/Types.hs
@@ -3,19 +3,19 @@ module Types where
 import Control.Concurrent (MVar)
 import Data.Time (UTCTime)
 
--- | Data type for an HTTP-like request
+-- | Request data type for modelling an HTTP request
 data Request = Request 
   { reqTimestamp :: UTCTime
   , reqContent   :: String
   }
   deriving (Show)
 
--- | Data type for an HTTP-like response
+-- | Response Data type for modelling an HTTP response
 data Response = Response
   { resTimestamp :: UTCTime
   , resContent   :: String
   }
   deriving (Show)
 
--- | Data type for the request queue
+-- | RequestQueue data type for modelling the queue of requests received by the server
 type RequestQueue = MVar [Request]
